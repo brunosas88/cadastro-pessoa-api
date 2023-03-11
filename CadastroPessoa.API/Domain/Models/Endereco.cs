963b49b422e8c9ea80117f895425fe7fb9be2f47
brunosas88 <bruno.sas@live.com>
2023-03-11 13:59:34 -0300

    Adiciona propriedades ao Endereco

--- CadastroPessoa.API/Domain/Models/Endereco.cs
+++ CadastroPessoa.API/Domain/Models/Endereco.cs
@@ -1,6 +1,13 @@
 ﻿namespace CadastroPessoa.API.Domain.Models
 {
 	public class Endereco
-	{
+	{		
+		public int Id { get; set; }
+		public string Cep { get; set; }
+		public string Numero { get; set; }
+		public string Logradouro { get; set; }
+		public string Bairro { get; set; }
+		public string Cidade { get; set; }
+		public string Complemento { get; set; }
 	}
 }
