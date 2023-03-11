5e0f5668a5b01865d4684952b69bc65f5994d2df
brunosas88 <bruno.sas@live.com>
2023-03-11 17:22:32 -0300

    Adiciona camada de servicos para Endereco
    Responsavel por transformar o dto em modelo
    e salva-lo no banco de dados

---
+++ CadastroPessoa.API/Services/EnderecoService.cs
@@ -0,0 +1,35 @@
+﻿using CadastroPessoa.API.Data;
+using CadastroPessoa.API.Domain.DTO;
+using CadastroPessoa.API.Domain.Models;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa.API.Services
+{
+	public class EnderecoService
+	{
+		private readonly DataContext _context;
+
+		public EnderecoService(DataContext context) {  _context = context; }
+
+		public async Task<Endereco> CriarEnderecoAsync(EnderecoDTO dto)
+		{
+			Endereco enderecoSalvo = ConverterDTOParaModelo(dto);
+			_context.Enderecos.Add(enderecoSalvo);
+			await _context.SaveChangesAsync();
+			return enderecoSalvo;
+		}
+
+		private Endereco ConverterDTOParaModelo(EnderecoDTO dto)
+		{
+			Endereco novoEndereco = new Endereco();
+			novoEndereco.Cep = dto.Cep;
+			novoEndereco.Numero = dto.Numero;
+			novoEndereco.Logradouro = dto.Logradouro;
+			novoEndereco.Bairro = dto.Bairro;
+			novoEndereco.Cidade = dto.Cidade;
+			novoEndereco.Complemento = dto.Complemento;
+
+			return novoEndereco;
+		}
+	}
+}
