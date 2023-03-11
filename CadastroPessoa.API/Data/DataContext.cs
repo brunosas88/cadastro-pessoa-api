0ff3f81aadd930a09a6eb0be5f8ace1d08ff3597
brunosas88 <bruno.sas@live.com>
2023-03-11 14:17:24 -0300

    Adiciona contexto

---
+++ CadastroPessoa.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CadastroPessoa.API.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoa.API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<Endereco> Enderecos { get; set; }
+		public DbSet<Pessoa> Pessoas { get; set; }		
+		public DbSet<PessoaJuridica> PessoasJuridicas { get; set; }
+		public DbSet<PessoaFisica> PessoasFisicas { get; set; }
+	}
+}
