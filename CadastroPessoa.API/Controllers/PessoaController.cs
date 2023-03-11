9e5ac8a8ea30e49262e7a47c1ab92d89edcfbbcf
brunosas88 <bruno.sas@live.com>
2023-03-11 17:25:27 -0300

    Adiciona controller para Pessoa
    Adicionado metodo post

---
+++ CadastroPessoa.API/Controllers/PessoaController.cs
@@ -0,0 +1,27 @@
+﻿using CadastroPessoa.API.Data;
+using CadastroPessoa.API.Domain.DTO;
+using CadastroPessoa.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CadastroPessoa.API.Controllers
+{
+	[ApiController]
+	[Route("/pessoas")]
+	public class PessoaController : ControllerBase
+	{
+		private readonly PessoaService _pessoaService;
+
+		public PessoaController(PessoaService pessoaService)
+		{
+			_pessoaService = pessoaService;
+		}
+
+		[HttpPost]
+		[Route("")]
+		public async Task<ActionResult<PessoaRequisicao>> Post([FromBody] PessoaRequisicao requisicao)
+		{
+			return await _pessoaService.SalvarPessoaAsync(requisicao);
+		}
+	}
+}
