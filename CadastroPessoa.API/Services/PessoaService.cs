31300eee08fcdc1f94b475f3d72a445999538b87
brunosas88 <bruno.sas@live.com>
2023-03-11 17:24:20 -0300

    Adiciona a camada de servico para Pessoa
    Responsavel por transformar o dto em modelo
    usando o servico de endereco para tratar seu dto
    e salvar o objeto corretamente no banco

---
+++ CadastroPessoa.API/Services/PessoaService.cs
@@ -0,0 +1,75 @@
+﻿using CadastroPessoa.API.Data;
+using CadastroPessoa.API.Domain.DTO;
+using CadastroPessoa.API.Domain.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CadastroPessoa.API.Services
+{
+	public class PessoaService
+	{
+		private readonly DataContext _context;
+		private readonly EnderecoService _enderecoService;
+
+		public PessoaService(DataContext context, EnderecoService enderecoService) 
+		{ 
+			_context = context;
+			_enderecoService = enderecoService;
+		}
+
+		public async Task<PessoaRequisicao> SalvarPessoaAsync(PessoaRequisicao requisicao)
+		{
+			Pessoa pessoaParaSalvar = await ConverterDTOParaModelo(requisicao);
+
+			if (pessoaParaSalvar is PessoaFisica pessoaFisica)			
+				_context.PessoasFisicas.Add(pessoaFisica);
+			
+			else if (pessoaParaSalvar is PessoaJuridica pessoaJuridica)
+				_context.PessoasJuridicas.Add(pessoaJuridica);
+
+			await _context.SaveChangesAsync();
+
+			return requisicao;
+		}
+
+		private async Task<Pessoa> ConverterDTOParaModelo (PessoaRequisicao requisicao)
+		{
+			Endereco enderecoSalvo = await _enderecoService.CriarEnderecoAsync(requisicao.Endereco);
+
+			if (requisicao.IdentificadorSocial.Length == 14)
+			{
+				PessoaJuridica novaPessoa = new PessoaJuridica();
+				novaPessoa.Nome = requisicao.Nome;
+				novaPessoa.Email = requisicao.Email;
+				novaPessoa.Telefone = requisicao.Telefone;
+				novaPessoa.EstaAtivo = true;
+				novaPessoa.CriadoEm = DateTime.Now;
+				novaPessoa.AtualizadoEm = DateTime.Now;
+				novaPessoa.Endereco = enderecoSalvo;
+				novaPessoa.EnderecoId = enderecoSalvo.Id;
+				novaPessoa.Cnpj = requisicao.IdentificadorSocial;
+				return novaPessoa;
+			}
+			else
+			{
+				PessoaFisica novaPessoa = new PessoaFisica();
+				novaPessoa.Nome = requisicao.Nome;
+				novaPessoa.Email = requisicao.Email;
+				novaPessoa.Telefone = requisicao.Telefone;
+				novaPessoa.EstaAtivo = true;
+				novaPessoa.CriadoEm = DateTime.Now;
+				novaPessoa.AtualizadoEm = DateTime.Now;
+				novaPessoa.Endereco = enderecoSalvo;
+				novaPessoa.EnderecoId = enderecoSalvo.Id;
+				novaPessoa.Cpf = requisicao.IdentificadorSocial;
+				return novaPessoa;
+			}
+		}
+
+
+
+	}
+}
