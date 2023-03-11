fa58848a9cf059b1a1e3c36c36ed1dadb647ce75
brunosas88 <bruno.sas@live.com>
2023-03-11 15:12:24 -0300

    Adiciona campo cpf/cnpj no dto Pessoa

--- CadastroPessoa.API/Domain/DTO/PessoaRequisicao.cs
+++ CadastroPessoa.API/Domain/DTO/PessoaRequisicao.cs
@@ -7,6 +7,7 @@ namespace CadastroPessoa.API.Domain.DTO
 		public string Nome { get; set; }	
 		public string Email { get; set; }
 		public string Telefone { get; set; }
+		public string IdentificadorSocial { get; set; }
 		public EnderecoDTO Endereco { get; set; }
 	}
 }
