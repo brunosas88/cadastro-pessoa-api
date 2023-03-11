1551e59297a36c2a501221d558545ac2064a8b36
brunosas88 <bruno.sas@live.com>
2023-03-11 15:09:45 -0300

    Adiciona dtos

---
+++ CadastroPessoa.API/Domain/DTO/PessoaRequisicao.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroPessoa.API.Domain.DTO
+{
+	public class PessoaRequisicao
+	{
+		public string Nome { get; set; }	
+		public string Email { get; set; }
+		public string Telefone { get; set; }
+		public EnderecoDTO Endereco { get; set; }
+	}
+}
