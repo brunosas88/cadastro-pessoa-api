e861d5956ae43f739334efbed35acc73c8b50d8a
brunosas88 <bruno.sas@live.com>
2023-03-17 13:33:27 -0300

    Atualiza migrations
    Adiciona o campo Uf em Endereco e atualiza
    nome de campo IdentificadorSocial para
    RegistroSocial em Pessoa

---
+++ CadastroPessoa.API/Migrations/20230317121309_adicionar-campo-Endereco.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroPessoa.API.Migrations
+{
+    public partial class adicionarcampoEndereco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Uf",
+                table: "Enderecos",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uf",
+                table: "Enderecos");
+        }
+    }
+}
