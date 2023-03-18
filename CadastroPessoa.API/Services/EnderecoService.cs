3cb6c484c74cc8257c36bed081b50b715ee0459e
brunosas88 <bruno.sas@live.com>
2023-03-18 10:42:13 -0300

    Adiciona utilizacao de repositorio Endereco

--- CadastroPessoa.API/Services/EnderecoService.cs
+++ CadastroPessoa.API/Services/EnderecoService.cs
@@ -1,21 +1,25 @@
 ﻿using CadastroPessoa.API.Data;
 using CadastroPessoa.API.Domain.DTO;
 using CadastroPessoa.API.Domain.Models;
+using CadastroPessoa.API.Repository;
 using System.Threading.Tasks;
 
 namespace CadastroPessoa.API.Services
 {
 	public class EnderecoService
 	{
-		private readonly DataContext _context;
+		private readonly EnderecoRepository _enderecoRepository;
 
-		public EnderecoService(DataContext context) {  _context = context; }
+		public EnderecoService(EnderecoRepository enderecoRepository) 
+		{  
+			_enderecoRepository = enderecoRepository;
+		}
 
 		public async Task<Endereco> CriarEnderecoAsync(EnderecoDTO dto)
 		{
-			Endereco enderecoSalvo = ConverterDTOParaModelo(dto);
-			_context.Enderecos.Add(enderecoSalvo);
-			await _context.SaveChangesAsync();
+			Endereco enderecoSalvo = ConverterDTOParaModelo(dto);			
+			enderecoSalvo = await _enderecoRepository.CadastrarEndereco(enderecoSalvo);
+
 			return enderecoSalvo;
 		}
 
